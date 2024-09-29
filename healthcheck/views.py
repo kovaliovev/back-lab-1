c8bdb5ef19bd31a59859f2c7e6a56ae00a9e400d
Yevhenii Kovalov <kovaliov.evgeniy11@gmail.com>
2024-09-29 19:26:18 +0300

    Implement healthcheck endpoint

--- healthcheck/views.py
+++ healthcheck/views.py
@@ -1,5 +1,15 @@
 from healthcheck import app
 
+from datetime import datetime
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/healthcheck", methods=["GET"])
+def healthcheck():
+    response = {
+        "status": "OK",
+        "current_date": datetime.now().isoformat()
+    }
+    return response, 200
